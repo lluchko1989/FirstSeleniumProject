d8a4eeb50dea9da02898e4ff3924069595ad5dbd
lluchko1989 <lluchko1989@gmail.com>
2026-04-13 08:44:58 +0200

    add1

--- src/test/java/OpenNewPage.java
+++ src/test/java/OpenNewPage.java
@@ -1,4 +1,5 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
@@ -9,18 +10,51 @@ public class OpenNewPage {
     WebDriver driver;
 
     @BeforeMethod
-    public void setUp (){
+    public void setUp() {
         WebDriverManager.chromedriver().browserVersion("146").setup();
         driver = new ChromeDriver();
         driver.navigate().to("https://demowebshop.tricentis.com/");
     }
+
     @Test
-    public void newPage (){
+    public void newPage() {
         System.out.println("Open page");
 
     }
+
+    @Test
+    public void find10Elements() {
+        driver.findElement(By.linkText("Books"));
+        driver.findElement(By.linkText("Computers"));
+        driver.findElement(By.linkText("Electronics"));
+        driver.findElement(By.linkText("Apparel & Shoes"));
+        driver.findElement(By.linkText("Digital downloads"));
+        driver.findElement(By.linkText("Jewelry"));
+        driver.findElement(By.linkText("Gift Cards"));
+
+        driver.findElement(By.id("small-searchterms"));
+        driver.findElement(By.id("newsletter-email"));
+        driver.findElement(By.linkText("Contact us"));
+    }
+    @Test
+    public void findTenElementsCss() {
+        driver.get("https://demowebshop.tricentis.com/");
+
+        driver.findElement(By.cssSelector("a[href='/register']"));
+        driver.findElement(By.cssSelector("a[href='/login']"));
+        driver.findElement(By.cssSelector("a[href='/cart']"));
+        driver.findElement(By.cssSelector("a[href='/wishlist']"));
+        driver.findElement(By.cssSelector("a[href='/books']"));
+        driver.findElement(By.cssSelector("a[href='/computers']"));
+        driver.findElement(By.cssSelector("a[href='/electronics']"));
+        driver.findElement(By.cssSelector("#small-searchterms"));
+        driver.findElement(By.cssSelector("#newsletter-email"));
+        driver.findElement(By.cssSelector("a[href='/contactus']"));
+    }
+
+
     @AfterMethod
-    public void tearDown (){
+    public void tearDown() {
         driver.quit();
     }
 
